e672a76f00a28715336c4264b8a12cb2c05d1cf9
eleanorcox <ec15613@my.bristol.ac.uk>
2019-03-03 22:47:19 +0000

    Improved http_server_maya.py to habdle post requests properly. However, done in a relatively hacky fashion, and having problems with getting values to return. Decided to define my own socket protocol instead of using http - will do this next

--- scripts/http_client.py
+++ scripts/http_client.py
@@ -1,7 +1,7 @@
 #################################################################
 # Testing http networking.
 # This is the CLIENT, run from a terminal.
-# Currently makes a GET request then a POST request
+# Makes a GET request then a POST request
 #################################################################
 import httplib
 import sys
@@ -14,10 +14,13 @@ data = response.read()
 print(data)
 connection.close()
 
+# After some experimentation it appears I need to open and close the connection
+# between each request. (Or, at least, this was a way around a lot of the problems
+# I was having)
 
 connection = httplib.HTTPConnection('localhost', 12345)
-#body = "CMD #polyCube -w 1 -h 1 -d 1 -sx 1 -sy 1 -sz 1 -ax 0 1 0 -cuv 4 -ch 1;" #MEL
-body = "polyCube -w 1 -h 1 -d 1 -sx 1 -sy 1 -sz 1 -ax 0 1 0 -cuv 4 -ch 1;" #Python
+#body = "CMD #polyCube -w 1 -h 1 -d 1 -sx 1 -sy 1 -sz 1 -ax 0 1 0 -cuv 4 -ch 1;" #Send to MEL script
+body = "polyCube -w 1 -h 1 -d 1 -sx 1 -sy 1 -sz 1 -ax 0 1 0 -cuv 4 -ch 1;" #Send to Python script
 headers = {"Content-Type": "text/plain"}
 connection.request("POST", "/", body, headers)
 response2 = connection.getresponse()
--- scripts/http_server_maya.py
+++ scripts/http_server_maya.py
@@ -1,13 +1,22 @@
 ####################################################################
 # Testing http networking.
 # Code run from inside Maya, so Maya acts as the http server.
-# GET and POST requests receved and replied to, although POST is not
-# currently working properly (i.e. changing whatever it is requested to)
+# GET and POST requests recieved. GET requests responded to correctly.
+# POST requests perform the correct command, but are not replying
+# to the server correctly. This seems to be a problem with commandPort,
+# as it interprets newline characters as separating out commands, and
+# http requests have newlines baked in.
+# To ovrcome this I am going to avoid http and write my own protocol
+# that works over sockets and is specific to my needs (e.g. I don't need
+# a lot of the information that comes with a http request such as language)
 ####################################################################
 
 import maya.cmds as cmds
 import pymel.core as pm
 import maya.mel as mel
+
+posting = False
+post_cmd_next = False
 
 pm.general.commandPort(name=":12345", pre="myServer", sourceType="mel", eo=True)
 
@@ -16,6 +25,7 @@ melproc = """
 global proc string myServer(string $str){
     $str = match( "^[^(\\r\\n)]*", $str );
     string $result = python(("myServer(\\"" + $str + "\\")"));
+    print($result);
     return $result;
 }
 """
@@ -28,14 +38,31 @@ def returnPage(message):
     return head + message
 
 def myServer(str):
+    global posting
+    global post_cmd_next
+
     buffer = str.split(" ")
-    #numTokens = len(buffer)
 
     if buffer[0]=="GET":        # GET request
         if buffer[1]=="/":
             return returnPage("Hello There!")
     elif buffer[0]=="POST":     # POST request
+        posting = True
         if buffer[1]=="/":
             return returnPage("General Kenobi")
+    elif posting and buffer[0]=="":     # Signifies the next input to the commandport is our command
+        post_cmd_next = True
+    elif posting and post_cmd_next:     # Runs the command
+        # str is a MEL COMMAND
+        print(str)
+        result_list = mel.eval(str)
+        result = ""
+        for i in result_list:
+            result = result + i + " "
+
+        posting = False
+        post_cmd_next = False
+
+        return returnPage(result)
 
 #pm.general.commandPort(name=":12345", cl=True)
