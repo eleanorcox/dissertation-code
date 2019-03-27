4cdf681e0cdfcb9cad794a3c921247e1346deccd
eleanorcox <ec15613@my.bristol.ac.uk>
2019-03-27 11:18:10 +0000

    Fixed poster day demo version. Added demo server

--- scripts/socket_client_maya_demo.py
+++ scripts/socket_client_maya_demo.py
@@ -1,110 +1,115 @@
 #################################################################
 # Socket client to Maya. Runs in terminal.
-#################################################################s
+#################################################################
 import numpy as np
 import socket
 import time
 import json
 import sys
 
+test_get = False
+test_put = True
+
 num_joint = 93
-num_frames = 8000    # ys has 39730 frames total or ~5.5 minutes (at 120fps). I am using a subset.
+num_frames = 4000    # ys has 39730 frames total or ~5.5 minutes (at 120fps). I am using a subset.
 
 # Number of frames per clip
 clip_0, clip_1, clip_2, clip_3, clip_4 = 57,37,50,46,51
 clip_5, clip_6, clip_7, clip_8, clip_9 = 35,35,48,32,56
-clip_10, clip_11, clip_12, clip_13     = 27,42,43,56
-clip_14, clip_15, clip_16, clip_17     = 44,49,48,30
+# clip_10, clip_11, clip_12, clip_13     = 27,42,43,56
+# clip_14, clip_15, clip_16, clip_17     = 44,49,48,30
 
 # Start frame of clip
 start_0, start_1, start_2, start_3, start_4 = 0,570,939,1439,1899
 start_5, start_6, start_7, start_8, start_9 = 2409,2759,3109,3589,4202
-start_10, start_11, start_12, start_13     = 4469,4739,5159,5589
-start_14, start_15, start_16, start_17     = 6149,6589,7053,7532
+# start_10, start_11, start_12, start_13     = 4469,4739,5159,5589
+# start_14, start_15, start_16, start_17     = 6149,6589,7053,7532
 
 clip_frames = 359
 
-def formatData(array):
-    string = np.array2string(array)
-    string = string.replace('[', '')
-    string = string.replace(']', '')
-    string = string.replace('\n', '')
-    return string
+if test_put:
+    print("Loading in data...")
+    full_ys = np.load('denormalised_y_full.npz')
+    ys = full_ys['Y']
 
-print("Loading in data...")
-full_ys = np.load('denormalised_y_full.npz')
-ys = full_ys['Y']
+    print("Extracting relevant data...")
+    joint_pos_full = np.ndarray(shape=(num_frames, num_joint))
+    xz_vel_full    = np.ndarray(shape=(num_frames, 2))
+    for i in range(num_frames):
+        y = ys[i]
+        joint_positions = y[32:32+num_joint]
+        joint_pos_full[i] = joint_positions
 
-print("Extracting relevant data...")
-joint_pos_full = np.ndarray(shape=(num_frames, num_joint))
-xz_vel_full    = np.ndarray(shape=(num_frames, 2))
-for i in range(num_frames):
-    y = ys[i]
-    joint_positions = y[32:32+num_joint]
-    joint_pos_full[i] = joint_positions
+        x_vel = y[0]
+        z_vel = y[1]
+        xz_vel_full[i] = [x_vel, z_vel]
 
-    x_vel = y[0]
-    z_vel = y[1]
-    xz_vel_full[i] = [x_vel, z_vel]
+    ### Massive hacks
+    ### Only using first 8 clips as gives slightly better animation
+    ### Hacky because only using for poster day demo
+    joint_pos_clips = np.ndarray(shape=(clip_frames, num_joint))
+    xz_vel_clips    = np.ndarray(shape=(clip_frames, 2))
+    start = 0
+    joint_pos_clips[start:start+clip_0] = joint_pos_full[start_0:start_0+clip_0]
+    xz_vel_clips[start:start+clip_0] = xz_vel_full[start_0:start_0+clip_0]
 
-### Massive hacks
-### Only using first 8 clips as gives slightly better animation
-### Hacky because only using for poster day demo
-joint_pos_clips = np.ndarray(shape=(clip_frames, num_joint))
-xz_vel_clips    = np.ndarray(shape=(clip_frames, 2))
-start = 0
-joint_pos_clips[start:start+clip_0] = joint_pos_full[start_0:start_0+clip_0]
-xz_vel_clips[start:start+clip_0] = xz_vel_full[start_0:start_0+clip_0]
+    start = start + clip_0
+    joint_pos_clips[start:start+clip_1] = joint_pos_full[start_1:start_1+clip_1]
+    xz_vel_clips[start:start+clip_1] = xz_vel_full[start_1:start_1+clip_1]
 
-start = start + clip_0
-joint_pos_clips[start:start+clip_1] = joint_pos_full[start_1:start_1+clip_1]
-xz_vel_clips[start:start+clip_1] = xz_vel_full[start_1:start_1+clip_1]
+    start = start + clip_1
+    joint_pos_clips[start:start+clip_2] = joint_pos_full[start_2:start_2+clip_2]
+    xz_vel_clips[start:start+clip_2] = xz_vel_full[start_2:start_2+clip_2]
 
-start = start + clip_1
-joint_pos_clips[start:start+clip_2] = joint_pos_full[start_2:start_2+clip_2]
-xz_vel_clips[start:start+clip_2] = xz_vel_full[start_2:start_2+clip_2]
+    start = start + clip_2
+    joint_pos_clips[start:start+clip_3] = joint_pos_full[start_3:start_3+clip_3]
+    xz_vel_clips[start:start+clip_3] = xz_vel_full[start_3:start_3+clip_3]
 
-start = start + clip_2
-joint_pos_clips[start:start+clip_3] = joint_pos_full[start_3:start_3+clip_3]
-xz_vel_clips[start:start+clip_3] = xz_vel_full[start_3:start_3+clip_3]
+    start = start + clip_3
+    joint_pos_clips[start:start+clip_4] = joint_pos_full[start_4:start_4+clip_4]
+    xz_vel_clips[start:start+clip_4] = xz_vel_full[start_4:start_4+clip_4]
 
-start = start + clip_3
-joint_pos_clips[start:start+clip_4] = joint_pos_full[start_4:start_4+clip_4]
-xz_vel_clips[start:start+clip_4] = xz_vel_full[start_4:start_4+clip_4]
+    start = start + clip_4
+    joint_pos_clips[start:start+clip_5] = joint_pos_full[start_5:start_5+clip_5]
+    xz_vel_clips[start:start+clip_5] = xz_vel_full[start_5:start_5+clip_5]
 
-start = start + clip_4
-joint_pos_clips[start:start+clip_5] = joint_pos_full[start_5:start_5+clip_5]
-xz_vel_clips[start:start+clip_5] = xz_vel_full[start_5:start_5+clip_5]
+    start = start + clip_5
+    joint_pos_clips[start:start+clip_6] = joint_pos_full[start_6:start_6+clip_6]
+    xz_vel_clips[start:start+clip_6] = xz_vel_full[start_6:start_6+clip_6]
 
-start = start + clip_5
-joint_pos_clips[start:start+clip_6] = joint_pos_full[start_6:start_6+clip_6]
-xz_vel_clips[start:start+clip_6] = xz_vel_full[start_6:start_6+clip_6]
+    start = start + clip_6
+    joint_pos_clips[start:start+clip_7] = joint_pos_full[start_7:start_7+clip_7]
+    xz_vel_clips[start:start+clip_7] = xz_vel_full[start_7:start_7+clip_7]
 
-start = start + clip_6
-joint_pos_clips[start:start+clip_7] = joint_pos_full[start_7:start_7+clip_7]
-xz_vel_clips[start:start+clip_7] = xz_vel_full[start_7:start_7+clip_7]
-
-print("Formatting data...")
-joint_pos_strings = np.ndarray(clip_frames, dtype=object)
-xz_vel_strings    = np.ndarray(clip_frames, dtype=object)
-for i in range(clip_frames):
-    joint_pos_strings[i] = formatData(joint_pos_clips[i])
-    xz_vel_strings[i]    = formatData(xz_vel_clips[i])
+    print("Formatting data...")
+    joint_pos_lists = np.ndarray(clip_frames, dtype=object)
+    xz_vel_lists    = np.ndarray(clip_frames, dtype=object)
+    for i in range(clip_frames):
+        joint_pos_lists[i] = joint_pos_clips[i].tolist()
+        xz_vel_lists[i]    = xz_vel_clips[i].tolist()
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 maya_address = ("localhost", 12345)
-print("Connecting to %s port %s" % maya_address)
+print "Connecting to %s port %s" % maya_address
 sock.connect(maya_address)
 
 try:
-    for i in range(clip_frames):
-        req_type = "PUT"
-        json_request = json.dumps({"RequestType": req_type, "JointPos": joint_pos_strings[i], "RootXformVels": xz_vel_strings[i]})
+    if test_get:
+        req_type = "GET"
+        json_request = json.dumps({"RequestType": req_type})
         sock.sendall(json_request)
         data = sock.recv(4096)
-        print("Received '%s'" % data)
-        time.sleep(0.1)
+        print "Received %s" % data
+
+    if test_put:
+        for i in range(clip_frames):
+            req_type = "PUT"
+            json_request = json.dumps({"RequestType": req_type, "JointPos": joint_pos_lists[i], "RootXformVels": xz_vel_lists[i]})
+            sock.sendall(json_request)
+            data = sock.recv(4096)
+            print "Received %s" % data
+            time.sleep(0.1)
 finally:
-    print("Closing socket")
+    print "Closing socket"
     sock.close()
---
+++ scripts/socket_server_maya_demo.py
@@ -0,0 +1,224 @@
+#################################################################
+# Runs from inside Maya.
+#################################################################
+
+import maya.cmds as cmds
+import pymel.core as pm
+import maya.mel as mel
+import json
+
+# May want to add bufferSize flag, size of buffer for commands and results. Default 4096.
+#pm.general.commandPort(name=":12345", pre="myServer", sourceType="mel", eo=True)
+
+anim_frames = 15
+
+# commandPort can only accept a MEL procedure as a prefix, so this acts as a wrapper for the python function myServer below.
+melproc = """
+global proc string myServer(string $str){
+    string $formatted = substituteAllString($str, "\\"", "'");
+    string $result = python(("myServer(\\"" + $formatted + "\\")"));
+    return $result;
+}
+"""
+
+mel.eval(melproc)
+
+# Names of joints stored here for easy access
+class Character():
+    def __init__(self):
+        self.root = getRootName()
+        self.joints = getJointNames([self.root], self.root)
+        self.velocities = initialiseVels()
+
+def myServer(str):
+    json_str = str.replace("'", '"')
+    request = json.loads(json_str)
+
+    if request["RequestType"] == "GET":
+        response = doGet()
+        #response = json.dumps({"test": [1,2,3,4]})
+        return response
+    elif request["RequestType"] == "PUT":
+        doPut(request)
+        return "PUT acknowledged"
+
+def doGet():
+    # Get path info
+    full_path_pos, path_middle_heights = getPathPos()
+    full_path_dir = getPathDir(full_path_pos)
+    full_path_height = getPathHeight(full_path_pos, full_path_dir, path_middle_heights)
+    # Get character info
+    initial_joint_pos = getJointPos()
+    initial_joint_vel = character.velocities
+    # Get gait info
+    full_gait = getGait()
+    # Format as JSON
+    response = formatGetJson(full_path_pos, full_path_dir, full_path_height, initial_joint_pos, initial_joint_vel, full_gait)
+    return response
+
+# Returns a list of [pos x, pos z] pairs
+def getPathPos():
+    # path = getPathName()
+    path = "curve1"             # Hardcoded
+    num_spans = cmds.getAttr(path + ".spans")
+    points_per_span = anim_frames / num_spans
+
+    # Assumes curve is uniformly parameterised (in most cases this is true)
+    full_path_pos = []
+    path_middle_heights = []
+    for i in range(num_spans):
+        for j in range(points_per_span):
+            param = i + float(j)/float(points_per_span)
+            pos = cmds.pointOnCurve(path, parameter=param, position=True)
+            full_path_pos.append([pos[0], pos[2]])  # Only x and z coords needed
+            path_middle_heights.append(pos[1])      # For use later in GetHeights function
+
+    return full_path_pos, path_middle_heights
+
+### THINK this is how directions are used in pfnn, not sure
+def getPathDir(full_path_pos):
+    full_path_dir = []
+    for i in range(len(full_path_pos) - 1):
+        x_dir = full_path_pos[i+1][0] - full_path_pos[i][0]
+        z_dir = full_path_pos[i+1][1] - full_path_pos[i][1]
+        direction = [x_dir, z_dir]
+        full_path_dir.append(direction)
+
+    full_path_dir.append([0,0]) # For final point on trajectory
+    return full_path_dir
+
+def getPathHeight(full_path_pos, full_path_dir, path_middle_heights):
+    # With high enough sampling for path (which is needed anyway for good animation) the
+    # left and right points are orthogonal to the direction of a point
+    # Right now can't be bothered to do the actual maths for this, so just going to set everything to 0 for testing
+    # TODO: implement properly later
+
+    full_path_height = []
+    for i in range(len(full_path_pos)):
+        full_path_height.append([0, 0, 0])
+    return full_path_height
+
+# Returns a LIST of joint positions
+# TODO: LOCAL joint positions not global!!
+def getJointPos():
+    joint_pos = []
+    for joint in character.joints:
+        joint_xform = cmds.xform(joint, worldSpace=True, query=True, translation=True)
+        joint_pos.append(joint_xform[0])
+        joint_pos.append(joint_xform[1])
+        joint_pos.append(joint_xform[2])
+    return joint_pos
+
+# TODO: full implementation from user input
+def getGait():
+    # For gait, 0=stand, 1=walk, 2=jog, 4=crouch, 5=jump, 6=unused (in pfnn)
+    # Want gait at each point along path - i.e. at each frame
+    # For now just set these to one of the values for testing, at a later date change this to get input from user
+    # Will need to format for X properly in loco.py
+    gait = []
+    for i in range(anim_frames):
+        gait.append(2)
+    return gait
+
+def formatGetJson(full_path_pos, full_path_dir, full_path_height, initial_joint_pos, initial_joint_vel, full_gait):
+    response = json.dumps({"AnimFrames": anim_frames, "PathPos": full_path_pos, "PathDir": full_path_dir, "PathHeight": full_path_height, "JointPos": initial_joint_pos, "JointVel": initial_joint_vel, "Gait": full_gait})
+    return response
+
+def doPut(request):
+    joint_pos, root_xform_x_vel, root_xform_z_vel = parsePut(request)
+    moveRootXform(root_xform_x_vel, root_xform_z_vel)
+    moveJoints(joint_pos)
+    setJointKeyframes()
+    updateFrame()
+
+def parsePut(request):
+    joint_pos = request["JointPos"]
+
+    vel = request["RootXformVels"]
+    root_xform_x_vel = float(vel[0])
+    root_xform_z_vel = float(vel[1])
+
+    return joint_pos, root_xform_x_vel, root_xform_z_vel
+
+def positionFromVelocity(initial_pos, velocity):
+    # time = 1/120    # frames?
+    time = 1
+    new_pos = (velocity * time) + initial_pos
+    return new_pos
+
+def moveRootXform(root_xform_x_vel, root_xform_z_vel):
+    root_xform = getRootXform()
+    new_x = positionFromVelocity(root_xform[0], root_xform_x_vel)
+    new_y = 0       # TODO: Hardcoded
+    new_z = positionFromVelocity(root_xform[2], root_xform_z_vel)
+    cmds.move(new_x, new_y, new_z, character.root, worldSpace=True)
+
+def moveJoints(joint_pos):
+    global character
+    root_xform = getRootXform()
+
+    for i in range(len(character.joints)):
+        x_offset = joint_pos[i*3]
+        y_offset = joint_pos[i*3+1]
+        z_offset = joint_pos[i*3+2]
+        x_xform = root_xform[0] + x_offset
+        y_xform = root_xform[1] + y_offset
+        z_xform = root_xform[2] + z_offset
+        cmds.move(x_xform, y_xform, z_xform, character.joints[i], worldSpace=True)
+
+def setJointKeyframes():
+    for joint in character.joints:
+        cmds.setKeyframe(joint, attribute="translate")
+
+def updateFrame():
+    now = cmds.currentTime(query=True)
+    cmds.currentTime(now + 1)
+
+def getJointNames(joints, joint):
+    children = cmds.listRelatives(joint)
+    if children is not None:
+        for child in children:
+            joints.append(child)
+            joints = getJointNames(joints, child)
+    return joints
+
+def getRootName():
+    joints = cmds.ls(type='joint')
+    x = joints[0]
+    found = False
+
+    while not found:
+        parent = cmds.listRelatives(x, parent=True)
+        if parent is not None:
+            x = parent[0]
+            root = parent[0]
+        elif parent is None:
+            found = True
+    return root
+
+def getRootXform():
+    ### TODO: HARDCODED NAMES
+    left_hip = "JOINT_LHipJoint"
+    right_hip = "JOINT_RHipJoint"
+
+    left_world_xform = cmds.xform(left_hip, worldSpace=True, query=True, translation=True)
+    right_world_xform = cmds.xform(right_hip, worldSpace=True, query=True, translation=True)
+    hip_world_xform = [0,0,0]
+    for i in range(len(left_world_xform)):
+        hip_world_xform[i] = (left_world_xform[i]+right_world_xform[i]) / 2
+
+    root_x = hip_world_xform[0]
+    root_y = 0                  # TODO: Hardcoded
+    root_z = hip_world_xform[2]
+
+    return [root_x, root_y, root_z]
+
+def initialiseVels():
+    velocities = []
+    for i in range(93):
+        velocities.append(0)
+    return velocities
+
+character = Character()
+
+#pm.general.commandPort(name=":12345", cl=True)
