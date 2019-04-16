f0eaf07c5e7c76495eab774f091e185bb47c32fa
eleanorcox <ec15613@my.bristol.ac.uk>
2019-04-16 21:50:54 +0100

    Maya: changed getPathPos function to get path positions for left, right and central points on the path. Improved implementation of getPathHeights, now gets heights from the closest vertex in the ground mesh. To improve add linear interpolation between vertices (for better height sampling)

--- scripts/maya.py
+++ scripts/maya.py
@@ -5,6 +5,7 @@
 import maya.cmds as cmds
 import maya.mel as mel
 import json
+import math
 
 anim_frames = 800
 server_on = False
@@ -47,9 +48,9 @@ def myServer(str):
 
 def doGet():
     # Get path info
-    path_pos = getPathPos()
+    left_pos, path_pos, right_pos = getPathPos()
     path_dir = getPathDir()
-    path_heights = getPathHeight()
+    path_heights = getPathHeights(left_pos, path_pos, right_pos)
     # Get character info
     joint_pos = getJointPos()
     joint_vel = getJointVel()
@@ -59,18 +60,38 @@ def doGet():
     response = formatGetJson(path_pos, path_dir, path_heights, joint_pos, joint_vel, path_gaits)
     return response
 
-# Returns a list of WORLD SPACE [pos x, pos z] pairs
+# Returns three lists of WORLD SPACE coordinates:
+# path_pos = [x, z] coordinates of points along the path
+# left_pos/right_pos = [x, z] coordinates of points 25cm to the left/right of the path, used later in getPathHeights
 def getPathPos():
     path = getPathName()
     point_dist = 1.0/anim_frames
+    unit = 25 # cm
     path_pos = []
+    left_pos = []
+    right_pos = []
 
     for i in range(anim_frames):
         param = i * point_dist
         pos = cmds.pointOnCurve(path, parameter=param, turnOnPercentage=True, position=True)
-        path_pos.append([pos[0], pos[2]])
+        tangent = cmds.pointOnCurve(path, parameter=param, turnOnPercentage=True, normalizedTangent=True)
+        normal = cmds.pointOnCurve(path, parameter=param, turnOnPercentage=True, normalizedNormal=True)
 
-    return path_pos
+        a_pos = [pos[0] + unit*normal[0], pos[1] + unit*normal[1], pos[2] + unit*normal[2]]
+        b_pos = [pos[0] - unit*normal[0], pos[1] - unit*normal[1], pos[2] - unit*normal[2]]
+        t_pos = [pos[0] + unit*tangent[0], pos[1] + unit*tangent[1], pos[2] + unit*tangent[2]]
+
+        d = (a_pos[0] - pos[0])*(t_pos[2] - pos[2]) - (a_pos[2] - pos[2])*(t_pos[0] - pos[0])
+
+        path_pos.append([pos[0], pos[2]])
+        if d < 0:
+            left_pos.append([b_pos[0], b_pos[2]])
+            right_pos.append([a_pos[0], a_pos[2]])
+        else:
+            left_pos.append([a_pos[0], a_pos[2]])
+            right_pos.append([b_pos[0], b_pos[2]])
+
+    return left_pos, path_pos, right_pos
 
 # Returns a list of WORLD SPACE [dir x, dir z] pairs
 def getPathDir():
@@ -87,25 +108,20 @@ def getPathDir():
 
 # TODO: implement properly later
 # Returns a list of WORLD SPACE [r, c, l] heights of the left, central and right sample points of the path
-def getPathHeight():
-    # path = getPathName()
-    # point_dist = 1.0/anim_frames
-    # path_norms = []
-    #
-    # for i in range(anim_frames):
-    #     param = i * point_dist
-    #     norm = cmds.pointOnCurve(path, parameter=param, turnOnPercentage=True, normalizedNormal=True)
-
-    # Right now can't be bothered to do this properly, so just going to set everything to 0 for testing
-    # for future: 25cm l/r in the test skeleton is roughly 10 units (guesstimate)
+def getPathHeights(left_pos, path_pos, right_pos):
+    ground_vtxs = getGroundVertices()
     path_heights = []
+
     for i in range(anim_frames):
-        path_heights.append([0, 0, 0])
+        vtx_r = getClosestVertex(right_pos[i], ground_vtxs)
+        vtx_c = getClosestVertex(path_pos[i], ground_vtxs)
+        vtx_l = getClosestVertex(left_pos[i], ground_vtxs)
+        path_heights.append([vtx_r[1], vtx_c[1], vtx_l[1]])
+
     return path_heights
 
 # Returns a list of WORLD SPACE joint positions
 def getJointPos():
-    root_xform_pos = getRootXformPos()
     joints_pos = []
 
     for joint in character.joints:
@@ -195,12 +211,37 @@ def getJointNames(joints, joint):
             joints = getJointNames(joints, child)
     return joints
 
-### Path ###
+### Path / Ground ###
 
 # TODO: get from user input
 def getPathName():
     path = "curve1"     # Hardcoded
     return path
+
+# TODO: get from user input
+def getGroundName():
+    ground = "pPlane1"  # Hardcoded
+    return ground
+
+def getGroundVertices():
+    ground = getGroundName()
+    vtx = cmds.xform(ground + '.vtx[*]', query=True, worldSpace=True, translation=True)
+    vertices = []
+    for i in range(len(vtx) / 3):
+        vertices.append([vtx[3*i + 0], vtx[3*i + 1], vtx[3*i + 2]])
+    return vertices
+
+def getClosestVertex(point, vertices):
+    closest = []
+    shortest_dist = float('inf')
+    for v in vertices:
+        xdist = (point[0] - v[0]) ** 2
+        zdist = (point[1] - v[2]) ** 2
+        euc_dist = math.sqrt(xdist + zdist)
+        if euc_dist < shortest_dist:
+            closest = v
+            shortest_dist = euc_dist
+    return closest
 
 ### Root Xform ###
 
@@ -254,11 +295,6 @@ def crossProduct(a, b):
          a[2]*b[0] - a[0]*b[2],
          a[0]*b[1] - a[1]*b[0]]
     return c
-
-def positionFromVelocity(initial_pos, velocity):
-    time = 1    # maybe need frames? so 1/60?
-    new_pos = (velocity * time) + initial_pos
-    return new_pos
 
 character = Character()
 
